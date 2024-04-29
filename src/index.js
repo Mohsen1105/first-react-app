6ac54e92c0e11c0d3c1fb4362b16b327be6d0315
unknown <mohsen110561@gmail.com>
2024-04-29 09:32:57 +0330

    jsx

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const element = React.createElement(
-    'div',
-    {id: 'app', className: 'container'},
-    'Hello World'
+// const element = React.createElement(
+//     'div',
+//     {id: 'app', className: 'container'},
+//     'Hello World'
+// )
+
+///jsx
+const element = (
+    <div id="app" className='container'>
+        Hello
+    </div>
 )
 
-const root = document.getElementsById('root')
 
+const root = document.getElementById('root');
+ 
 ReactDOM.render(element, root)
 

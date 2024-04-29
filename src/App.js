27bfff32caf40028a667c9f17c554f381140c0b8
unknown <mohsen110561@gmail.com>
2024-04-29 13:14:08 +0330

    message

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 
 import Product from "./components/Products"
 
-function App(){
+//function App(){
+const App = () => {
     return(
     <div className='container'>
+        {/* <Product />
+        <Product />
+        <Product /> */}
+
+        {/* using props as attribute */}
         <Product title='Book 1'/>
         <Product title='Book 2'/>
         <Product title='Book 3'/>
+
     </div>
 )
 }
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,7 +1,24 @@
 import './Product.css'
 
-function Product(props){
-    return <div className='product'>{props.title}</div>
-}
+
+// function Product(){
+//     return <div className='product'>Product</div>
+// }
+
+// //dynamic components
+// //function Product(props){
+// const Product = (props) => {
+//     return <div className='product'>{props.title}</div>
+// }
+
+//object destructing (implicit props)
+    const Product = ({title}) => {
+        return <div className='product'>
+            <div>{title}</div>
+            <div >
+                <button className='btn'>Title</button>
+            </div>
+        </div>
+    }
 
 export default Product

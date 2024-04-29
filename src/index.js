512c66f582f9305c546ecc0844692c4dfe682837
unknown <mohsen110561@gmail.com>
2024-04-29 09:45:17 +0330

    using component

--- src/index.js
+++ src/index.js
@@ -1,21 +1,33 @@
-import React from 'react'
+// react 17 dont need to use import react
+//import React from 'react'
 import ReactDOM from 'react-dom'
 
+
+// 1
 // const element = React.createElement(
 //     'div',
 //     {id: 'app', className: 'container'},
 //     'Hello World'
 // )
 
-///jsx
-const element = (
+
+// /// 2.jsx
+// const element = (
+//     <div id="app" className='container'>
+//         Hello
+//     </div>
+// )
+//ReactDOM.render(element, root)
+
+
+// 3. functions (for using components we must create function)
+function App(){
+    return(
     <div id="app" className='container'>
         Hello
     </div>
 )
+}
 
-
-const root = document.getElementById('root');
- 
-ReactDOM.render(element, root)
+ReactDOM.render(<App/>, document.getElementById('root'))
 
